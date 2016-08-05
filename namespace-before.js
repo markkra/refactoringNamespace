574e2e87a5a39c99c27db426f7f6c1ac1a7bb122
Mark Kramer <mkramer@expedia.com>
2016-08-05 08:07:20 -0700

    Setup for debugging. Add scope and make it such that it can run in node.js or the browser.

--- namespace-before.js
+++ namespace-before.js
@@ -1,21 +1,27 @@
-var NGAT = NGAT || {};
-NGAT.namespace = function () {
+(function IIFE(global) {
+
+  global.NGAT = global.NGAT || {};
+  
+  global.NGAT.namespace = function () {
     var a = arguments, o, i = 0, j, d, arg;
 
     for (; i < a.length; i++) {
-        o = window;
-        arg = a[i];
-        if (arg.indexOf(".") > -1) {
-            d = arg.split(".");
-            for (j = 0; j < d.length; j++) {
-                o[d[j]] = o[d[j]] || {};
-                o = o[d[j]];
-            }
+      o = global;
+      arg = a[i];
+      if (arg.indexOf(".") > -1) {
+        d = arg.split(".");
+        for (j = 0; j < d.length; j++) {
+          o[d[j]] = o[d[j]] || {};
+          o = o[d[j]];
         }
-        else {
-            o[arg] = o[arg] || {};
-            o = o[arg];
-        }
+      }
+      else {
+        o[arg] = o[arg] || {};
+        o = o[arg];
+      }
     }
     return o;
-};
+  };
+
+})(this);
+
