4f8c550d96e9c2a18bd1adcfad3ff0cf97da6e30
Mark Kramer <mkramer@expedia.com>
2016-08-08 08:01:26 -0700

    Starting point - raw NGAT.namespace()

--- namespace-before.js
+++ namespace-before.js
@@ -1,27 +1,22 @@
-(function IIFE(global) {
 
-  global.NGAT = global.NGAT || {};
-  
-  global.NGAT.namespace = function () {
+var NGAT = NGAT || {};
+NGAT.namespace = function () {
     var a = arguments, o, i = 0, j, d, arg;
 
     for (; i < a.length; i++) {
-      o = global;
-      arg = a[i];
-      if (arg.indexOf(".") > -1) {
-        d = arg.split(".");
-        for (j = 0; j < d.length; j++) {
-          o[d[j]] = o[d[j]] || {};
-          o = o[d[j]];
+        o = window;
+        arg = a[i];
+        if (arg.indexOf(".") > -1) {
+            d = arg.split(".");
+            for (j = 0; j < d.length; j++) {
+                o[d[j]] = o[d[j]] || {};
+                o = o[d[j]];
+            }
         }
-      }
-      else {
-        o[arg] = o[arg] || {};
-        o = o[arg];
-      }
+        else {
+            o[arg] = o[arg] || {};
+            o = o[arg];
+        }
     }
     return o;
-  };
-
-})(this);
-
+};
